b57d6784a162a7f15749d972ee3ee47eb3d26055
Ganesh <42795619+ganesh-28@users.noreply.github.com>
2019-09-26 22:24:10 +0530

    Update ssh_start.sh

--- ssh_start.sh
+++ ssh_start.sh
@@ -2,6 +2,6 @@
 # Docker Entrypoint script.
 # Start the ssh server
 /etc/init.d/ssh restart
-echo "Welcome to Docker Container with Ansible!"
+echo "Welcome to Docker Container with Ansible running!"
 # Execute the CMD
 exec "$@"
